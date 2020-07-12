b4365985392cc5a7d34a55c3641b72f21a1ddeb5
AkiraTerui <hanabi0111@gmail.com>
2020-07-11 20:47:06 -0400

    Edits: minor corrections

--- figure05b_phi_gaussian.R
+++ figure05b_phi_gaussian.R
@@ -6,16 +6,16 @@
   rm(list=ls(all.names=T))
   source("datasort02_gaussian.R")
 
-## axis titles
+  ## axis titles
   xtitle <- expression("True dispersal parameter"~sigma[true]~"(m)")
   ytitle <- expression("Estimated recpature probability"~phi[est])
 
-## panel label
+  ## panel label
   dat$phi.label <- sprintf('phi=="%.1f"', dat$PHI); dat$phi_f <- factor(dat$phi.label, levels = unique(dat$phi.label) )
   dat$N.label <- sprintf('N=="%i"', dat$N); dat$N_f <- factor(dat$N.label, levels = unique(dat$N.label) )
   dat$L.label <- sprintf('L=="%i"', dat$LEN); dat$L_f <- factor(dat$L.label, levels = unique(dat$L.label) )
-
-## plot colors
+  
+  ## plot colors
   COL <- c(rgb(0,0,0), rgb(0,0.3,0.7),rgb(1,0,0))
 
 # Plot ----
