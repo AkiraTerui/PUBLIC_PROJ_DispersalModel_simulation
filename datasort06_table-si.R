796683ed0322a7c274d0edcebba90f5984468d92
AkiraTerui <hanabi0111@gmail.com>
2020-07-28 13:50:36 -0400

    Create datasort06_table-si.R

---
+++ datasort06_table-si.R
@@ -0,0 +1,32 @@
+
+# Read library
+  library(tidyverse)
+  library(knitr)
+
+# Read data
+  rm(list = ls(all.names = T))
+  source("datasort01_laplace.R")
+  datl <- dat %>% mutate(Kernel = "Laplace")
+  
+  source("datasort02_gaussian.R")
+  datg <- dat %>% mutate(Kernel = "Gaussian")
+  
+  dat <- bind_rows(datl, datg)
+
+# Data format
+  dat_table = dat %>%
+    group_by(Kernel, N, LEN, PHI, THETA, model_code) %>%
+    summarize(Bias = round(mean(bias),1) ) %>%
+    pivot_wider(names_from = model_code, values_from = Bias)
+  
+  theta <- sort(unique(dat_table$THETA))
+  DL <- list(NULL)
+  for(i in 1:length(theta)){
+    DL[[i]] = dat_table %>%
+      filter(THETA == theta[i]) %>%
+      arrange(Kernel) %>%
+      kable(col.names = c("Kernel", "$N$", "$L$", "$\\phi$", "$\\theta_{true}$",
+                          "$\\%~bias_{simple}$", "$\\%~bias_{trunc}$", "$\\%~bias_{disp-obs}$"),
+            format = "markdown")
+    names(DL)[i] <- paste0("Table S", i)
+  }
